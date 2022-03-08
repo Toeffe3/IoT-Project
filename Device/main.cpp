11593e22c67e8bf96d0c16b9fd21ed6ffcfb6d8b
Toeffe3 <victor.jacobsen@outlook.dk>
2022-03-08 14:13:50 +0100

    Solution setup

---
+++ Device/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Device.cpp
+ *
+ * Created: 08-03-2022 14:07:03
+ * Author : dense
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ Hub/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Hub.cpp
+ *
+ * Created: 08-03-2022 14:11:39
+ * Author : dense
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
