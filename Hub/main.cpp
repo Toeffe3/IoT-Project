d9b67ddd56f197dc4017210e9a1b9ccab8f09257
Luhning <Luhning@users.noreply.github.com>
2022-03-30 13:19:21 +0000

    🎨 Formattede code

--- Hub/main.cpp
+++ Hub/main.cpp
@@ -7,25 +7,19 @@
 #include "header.h"
 #include "rtc.h"
 
-void init(void);
+void init (void);
 
 int main (void) {
-	int ticks;
-	
-	
+  int ticks;
+
   // Init
-	init();
+  init ( );
 
   // Loop
   while (1) {}
 }
 
-void init(){
-	rtc_init(true);	
-}
-
+void init ( ) { rtc_init (true); }
 
 // Mayby AVR32_RTC_ISR
-ISR(AVR32_RTC_IRQ ){
-	ticks++;
-}
+ISR (AVR32_RTC_IRQ) { ticks++; }
