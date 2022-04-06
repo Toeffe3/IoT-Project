28bf54abfd6c235cbac3af575824171951e44a1b
Luhning <100916763+Luhning@users.noreply.github.com>
2022-04-06 13:30:45 +0200

    ...

--- Hub/main.cpp
+++ Hub/main.cpp
@@ -5,13 +5,15 @@
  */
 
 #include "header.h"
+#include "tc.h"
 #include "gpio.h"
 #include "rtc.h"
+#include "usb.h"
 
 void init (void);
 
 int main (void) {
-  int ticks;
+  uint32_t ticks;
 
   // Init
   init ( );
@@ -21,6 +23,8 @@ int main (void) {
 }
 
 void init ( ) { 
+	tc_init();
+	usb_init();
 	gpio_init ();
 	rtc_init (OSC_32KHZ, PRESCALER, TOP_VAL); }
 
---
+++ Hub/src/usb.h
@@ -0,0 +1,16 @@
+/*
+ * usb.h
+ *
+ * Created: 06-04-2022 11:44:53
+ *  Author: Tobia
+ */ 
+
+
+#ifndef USB_H_
+#define USB_H_
+
+
+
+
+
+#endif /* USB_H_ */
