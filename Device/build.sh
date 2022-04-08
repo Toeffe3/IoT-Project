416e9ca69cf1ef366ebc3919c20fd9f3d45d7ccd
Toeffe3 <victor.jacobsen@outlook.dk>
2022-04-08 10:35:17 +0200

    fix build.sh

--- Device/build.sh
+++ Device/build.sh
@@ -1,4 +1,14 @@
-clear
-avr-gcc --save-temps -Os -Wall -g -mmcu=attiny167 --language=c -o output/device.bin --include=src/USI.c main.c
-hexdump output/device.bin --canonical
-sudo avrdude -e -v -v -v -P usb -p t167 -c usbasp -C avrdude.conf -U flash:w:"output/device.bin"
+#*!/bin/bash
+mkdir -p output
+cd output
+while test $# -gt 0; do
+	if [ $1 = "--compile" ]; then
+		avr-gcc --save-temps -Os -Wall -g -mmcu=attiny167 --language=c -o odevice.bin --include=../src/USI.c --include-directory=/usr/lib/avr/include/ ../main.c
+	elif [ $1 = "--list-output" ]; then
+		readelf -a device.bin
+		ls
+	elif [ $1 = "--upload" ]; then 
+		sudo avrdude -e -v -v -v -P usb -p t167 -c usbasp -C avrdude.conf -U flash:w:"device.bin"
+	fi
+	shift #shift next argument into $1
+done
