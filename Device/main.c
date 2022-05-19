9667821da1b21d6411b5eaf7b17d9969b44116f3
Toeffe3 <Toeffe3@users.noreply.github.com>
2022-05-19 16:10:19 +0000

    🎨 Formattede code

--- Device/main.c
+++ Device/main.c
@@ -8,20 +8,20 @@ int main (void) {
   // Setup
   ticks_init ( );
   LED_init ( );
-  LDR_init();
-  com_init();
+  LDR_init ( );
+  com_init ( );
   // Loop
   uint8_t i = 0;
   while (1) {
-	//com_send_char('a' + i++);
-	uint16_t data = LDR_read();
-	com_send_num(data);
-	_delay_ms(1000);
-	  
+	// com_send_char('a' + i++);
+	uint16_t data = LDR_read ( );
+	com_send_num (data);
+	_delay_ms (1000);
+
 	// PORTA = 0xff;
 	//_delay_ms(1000);
 	// PORTA = 0x00;
-	//SSD_write (i++ % 16);
+	// SSD_write (i++ % 16);
 	//_delay_ms (1000);
   }
 }
